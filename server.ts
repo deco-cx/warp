797b0a1c37b2566dc5b587993ff5ba284d0930f6
Marcos Candeia <marrcooos@gmail.com>
2025-04-15 15:04:48 -0300

    Remove @master annotation from mcp run
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- server.ts
+++ server.ts
@@ -220,7 +220,7 @@ export class Warp implements DurableObject {
 }
 
 const RUN_SCRIPT =
-  "https://denopkg.com/deco-sites/mcp@master/run.ts";
+  "https://denopkg.com/deco-sites/mcp/run.ts";
 export default {
   fetch(req: Request, env: { WARP: DurableObjectNamespace }) {
     const reqUrl = new URL(req.url);
